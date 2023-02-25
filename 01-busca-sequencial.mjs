046cb7963b2665f70015844f820518f24ab1ba76
XandyGomes <x3.computadores@hotmail.com>
2023-02-25 11:06:38 -0300

    Aula.25.02.2023

---
+++ 01-busca-sequencial.mjs
@@ -0,0 +1,21 @@
+/*
+    ALGORITMO DE BUSCA SEQUENCIAL
+
+    Percorre o vetor, verificando se cada um dos elementos corresponde
+    ao valor de busca. Ao encontrar uma correspondência, retorna a posição
+    onde o valor de busca está no vetor.
+
+    Caso o valor de busca não exista no vetor, retorna o valor convencional
+    -1.
+*/
+
+function buscaSequencial(vetor, valorBusca){
+    for(let i=0; i < vetor.length; i++){
+        if(vetor[i] === valorBusca) return i
+    }
+    return -1
+}
+
+const frutas = ["laranja", "maça", "uva", "pera", "jabuticaba", "limão"]
+
+console.log("A posição da uva é: ", buscaSequencial(frutas, "uva"))
