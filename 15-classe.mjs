d867501b7e99c73d8dd03d69c473987ee6b50db2
XandyGomes <x3.computadores@hotmail.com>
2023-04-29 11:27:03 -0300

    Aula.29.04.2023

--- 15-classe.mjs
+++ 15-classe.mjs
@@ -4,8 +4,20 @@ class FormaGeometrica {
     #tipo
 
     constructor(base, altura, tipo){
+
+        if(typeof base !== 'number' || base <= 0){
+            throw new Error('ERRO: a base precisa ser numérica e maior que zero')
+        }
         this.#base = base
+
+        if(typeof altura !== 'number' || altura <= 0){
+            throw new Error('ERRO: a altura precisa ser numérica e maior que zero')
+        }
         this.#altura = altura
+
+        if (! ['R', 'T', 'E'].includes(tipo)){
+            throw new Error('ERRO: o tipo precisa ser R, T ou E')
+        }
         this.#tipo = tipo
     }
 
@@ -20,13 +32,33 @@ class FormaGeometrica {
     }
 
     set base(valor){
-
-        if(typeof valor !== 'number' || valor <= 0){
-            throw new Error('ERRO: a base precisa ser numérica e maior que zero')
-        }
         this.#base = valor
     }
     
+    set altura(valor){
+        this.#altura = valor
+    }
 
-    set    
+    set tipo(valor){
+        this.#tipo = valor
+    }
+
+    calcularArea(){
+        switch(this.tipo){
+            case 'R': // Retângulo
+                return this.base * this.altura
+            case 'T': // Triângulo
+                return this.base * this.altura / 2
+            case 'E': // Elipse
+                return (this.base / 2) * (this.altura / 2) * Math.PI
+        }
+    }
 }
+
+
+
+let obj1 = new FormaGeometrica(2, 20, 'R')
+let obj2 = new FormaGeometrica(-11, 30, 'T')
+let obj3 = new FormaGeometrica(4, 40, 'E')
+
+console.log(obj2.calcularArea())
