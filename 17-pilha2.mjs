0a743ea83a5b115c5e1b0c17e14c3fd5ddb8eca5
XandyGomes <x3.computadores@hotmail.com>
2023-06-15 19:13:15 -0300

    Aula.15.06.2023

--- 17-pilha2.mjs
+++ 17-pilha2.mjs
@@ -1,28 +1,21 @@
 import Stack from './lib/Stack.mjs'
 
 let pilha = new Stack()
-
-console.log("Está vazia?\nR:", pilha.isEmpty)
+console.log(pilha.print())
+console.log('Está vazia?', pilha.isEmpty)
 
 pilha.push(35)
-pilha.push(80)
-pilha.push(7)
-pilha.push(15)
-
-console.log("Está vazia?\nR:", pilha.isEmpty)
-
+pilha.push(77)
+pilha.push(19)
 console.log(pilha.print())
+console.log('Está vazia?', pilha.isEmpty)
 
 let removido = pilha.pop()
-console.log("Elemento removido:", removido)
+console.log({removido}, pilha.print())
 
+let ultimo = pilha.peek()
+console.log({ultimo}, pilha.print())
 
-pilha.push(20)
-
-console.log(pilha.print())
-
-console.log("Último elemento:\nR:", pilha.peek())
-
-//tentar inserir no início da pilha
-// pilha.unshift(99)
-// console.log(pilha.print())
+// Tentativa de inserção no início da pilha
+//pilha.unshift(90)
+//console.log(pilha.print())
--- 18-pilha3.mjs
+++ 18-pilha3.mjs
@@ -1,19 +1,30 @@
-import Stack from './lib/Stack.mjs';
+/* 
+   PALÍNDROMO: texto que pode ser lido de trás para frente e
+   continua idêntico
+*/
 
-let frase= 'Socorram-me, subi no ônibus em Marrocos'
+import Stack from './lib/Stack.mjs'
 
-let palindromo = new Stack()
+let frase = 'Socorram-me, subi no ônibus em Marrocos'
 
-for(let i=0; i<frase.length; i++){
-    palindromo.push(frase.charAt(i))
+/*
+    Problema: inverter a frase para verificar se é um palíndromo
+*/
+
+let pilha = new Stack()
+
+// Percore a frase e coloca cada uma das letras dela
+// dentro da pilha
+for(let i = 0; i < frase.length; i++) {
+    pilha.push(frase.charAt(i))     // Insere no final da pilha
 }
 
-console.log(palindromo.print())
+console.log(pilha.print())
 
-let fraseInversa = ''
+let reverso = ''
 
-while(! palindromo.isEmpty){
-    fraseInversa += palindromo.pop()
+while(! pilha.isEmpty) {
+    reverso += pilha.pop()          // Retira do final da pilha
 }
 
-console.log(fraseInversa)
+console.log(reverso)
