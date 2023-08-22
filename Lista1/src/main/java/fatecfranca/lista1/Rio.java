953049d8c28770d397d1f6b0ace42456567c9f88
mateushilariodias <mateusilariodias@gmail.com>
2023-08-22 19:47:13 -0300

    delete and create new Rio.java, and update it

--- Lista1/src/main/java/fatecfranca/lista1/Rio.java
+++ Lista1/src/main/java/fatecfranca/lista1/Rio.java
@@ -9,7 +9,49 @@ package fatecfranca.lista1;
  * @author mateu
  */
 public class Rio {
+
     String nome;
     float nível;
-    Boolean poluído ;
+    Boolean poluído;
+
+    public Rio() {
+
+    }
+
+    public void Rio(String nome, float nível, Boolean poluído) {
+        this.nome = nome;
+        this.nível = nível;
+        this.poluído = poluído;
+    }
+
+    public void chover(float x) {
+        this.nível = nível + x;
+        System.out.println("Aumentou o nível do rio!");
+    }
+
+    public void ensolarar(float y) {
+        if (this.nível >= y) {
+            this.nível = nível - y;
+            System.out.println("Baixou o nível do rio!");
+        } else {
+            System.out.println("Secou!");
+        }
+    }
+
+    public void limpar() {
+        this.poluído = false;
+        System.out.println(this.poluído);
+    }
+
+    public void sujar() {
+        this.poluído = true;
+        System.out.println(this.poluído);
+    }
+
+    public void mostrar() {
+        System.out.println("Nome do rio:" + this.nome
+                + "\n Nível do rio:" + this.nível
+                + "\n Poluído:" + this.poluído);
+    }
+
 }
