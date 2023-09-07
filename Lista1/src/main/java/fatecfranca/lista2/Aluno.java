1f26f4a8ed58788911e8dce28fdd2cc86306bb4b
mateushilariodias <mateusilariodias@gmail.com>
2023-09-07 18:42:29 -0300

    add Aluno.java and TestaAluno.java

--- Lista1/src/main/java/fatecfranca/lista2/Aluno.java
+++ Lista1/src/main/java/fatecfranca/lista2/Aluno.java
@@ -9,7 +9,6 @@ package fatecfranca.lista2;
  * @author mateu
  */
 public class Aluno {
-
     private int numeroAluno, idade;
     private String nome;
     private float p1, p2;
@@ -22,9 +21,8 @@ public class Aluno {
         this.setP2(p2);
     }
 
-    public Aluno() {
-    }
-
+    public Aluno() {}
+    
     public int getNumeroAluno() {
         return numeroAluno;
     }
@@ -33,7 +31,7 @@ public class Aluno {
         if (String.valueOf(numeroAluno).length() == 6) {
             this.numeroAluno = numeroAluno;
         } else {
-            System.out.println("Número do alluno inválido!");
+            System.out.println("Número da conta inválido!");
         }
     }
 
@@ -85,17 +83,13 @@ public class Aluno {
             System.out.println("A nota da p2 não pode ser negativa!");
         }
     }
-
-    public final void notaFinal() {
-        if (this.p1 >= 0 && this.p2 >= 0) {
-            System.out.println((this.p1 + this.p2) / 2);
-        } else {
-            System.out.println("As notas p1 e p2 devem ser positivas");
-        }
+    
+    public float notaFinal() {
+        return ((this.p1 + this.p2) /2);
     }
 
     public String dadosAluno() {
         return "Aluno{" + "numeroAluno=" + numeroAluno + ", idade=" + idade + ", nome=" + nome + '}';
     }
-
+    
 }
